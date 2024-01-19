4f44c18de3a626f95b22a61faa3653820e8f4fd3
sBucka <118976633+sBucka@users.noreply.github.com>
2024-01-19 01:38:12 +0100

    Other

---
+++ HTML/Projects/Other/other.js
@@ -0,0 +1,4 @@
+// Buttons
+document.getElementById("hover-grid").addEventListener("click", function () {
+  window.location.href = "Hover-grid/hover-grid.html";
+});
---
+++ HTML/Projects/main.js
@@ -0,0 +1,12 @@
+// Buttons
+document.getElementById("games").addEventListener("click", function () {
+  window.location.href = "Games/games.html";
+});
+
+document.getElementById("simon").addEventListener("click", function () {
+  window.location.href = "Simon/simon.html";
+});
+
+document.getElementById("other").addEventListener("click", function () {
+  window.location.href = "Other/other.html";
+});
