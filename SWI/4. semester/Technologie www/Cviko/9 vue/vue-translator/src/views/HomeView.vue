5397d88512b0ad1c4ca0efada8e912a6f91f3b5f
sBucka <118976633+sBucka@users.noreply.github.com>
2024-04-04 14:20:08 +0200

    Refactor router and update home view

--- SWI/4. semester/Technologie www/Cviko/9 vue/vue-translator/src/views/HomeView.vue
+++ SWI/4. semester/Technologie www/Cviko/9 vue/vue-translator/src/views/HomeView.vue
@@ -1,9 +1,67 @@
 <script setup>
-import TheWelcome from '../components/TheWelcome.vue'
+//import TheWelcome from '../components/TheWelcome.vue'
+import { ref } from "vue";
+
+const userInput = ref("");
+const translationResult = ref("");
+
+const translate = () => {
+  console.log(userInput.value);
+  const url = `https://api.mymemory.translated.net/get?q=${userInput.value}&langpair=en|cs`;
+  if (!userInput.value || userInput.value.length < 2) {
+    return;
+  }
+  fetch(url)
+    .then((response) => response.json())
+    .then((data) => {
+      console.log(data);
+      translationResult.value = data.responseData.translatedText;
+    })
+    .catch((error) => {
+      console.error("Error:", error);
+    });
+};
 </script>
 
 <template>
   <main>
-    <TheWelcome />
+    <h1>Welcome to the Vue Translator</h1>
+    <input
+      v-model="userInput"
+      type="text"
+      placeholder="Text for translation..."
+    />
+    <button @click="translate">Translate</button>
+    {{ userInput }} --> {{ translationResult }}
+
+
   </main>
 </template>
+
+<style scoped>
+
+main {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  height: 100vh;
+}
+
+input {
+  margin: 1rem;
+  padding: 0.5rem;
+  font-size: 1rem;
+}
+
+
+
+button {
+  padding: 0.5rem 1rem;
+  font-size: 1rem;
+  background-color: var(--color-primary);
+  color: white;
+  border: none;
+  cursor: pointer;
+}
+</style>
